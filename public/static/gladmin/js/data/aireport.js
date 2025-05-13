83cdc7a578e6786b207a9d0dab51311cedcafc8f
jinike <jinike123@gmail.com>
2025-05-13 16:31:13 +0800

    新增拒绝提现

--- public/static/gladmin/js/data/aireport.js
+++ public/static/gladmin/js/data/aireport.js
@@ -27,7 +27,16 @@ define(["jquery", "easy-admin"], function ($, ea,Vue) {
                     {field: 'first_charge_amount', minWidth: 0, title: '首冲金额', search: false},
                     {field: 'repeat_charge_amount', minWidth: 0, title: '复充金额', search: false},
                     {field: 'total_charge_amount', minWidth: 0, title: '总充金额', search: false},
-                    {field: 'total_settlement_amount', minWidth: 0, title: '结算金额', search: false},
+                    {
+                        field: 'total_settlement_amount',
+                        minWidth: 0,
+                        title: '复充金额',
+                        search: false,
+                        templet: function (row) {
+                            return parseFloat(row.total_settlement_amount).toFixed(2);
+                        }
+                    },
+
                     {field: 'total_agent_amount', minWidth: 0, title: '代理收益', search: false},
                     {field: 'platform_profit', minWidth: 0, title: '平台收益', search: false},
                     {field: 'total_coin_consumed', minWidth: 0, title: '点数消耗', search: false},
